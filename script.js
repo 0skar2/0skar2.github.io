5d3a1f3cd97aedabfe7edc9f742c53519c52fcc2
0skar2 <110235720+0skar2@users.noreply.github.com>
2024-10-15 17:26:41 +0200

    test

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function idfk() {
 function mm() {
     alert("idfk what to say here")
 }
+
+function openIframeAsWindow(url) {
+    const newWindow = window.open(url, 'myIframe', 'width=600,height=400');
+    if (newWindow) {
+        newWindow.onload = function() {
+            // Access the iframe's content
+            const iframe = document.getElementById('myIframe');
+            iframe.src = url; // Load the URL into the iframe
+        };
+    } else {
+        alert('Please allow popups for this website');
+    }
+}
