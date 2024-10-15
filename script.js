904bcaf7d56a1e9c727c756cd9bbc6dbc9a89bce
0skar2 <110235720+0skar2@users.noreply.github.com>
2024-10-15 20:11:03 +0200

    stuff

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-const wind = window.open("./digitalsandcat.html", "idfk", 'width=500,height=500');
-
 function idfk() {
     alert("hii")
 }
 function mm() {
     alert("idfk what to say here")
-}
-
-function moveWindBy() {
-    wind.moveBy(10, 10);
 }
